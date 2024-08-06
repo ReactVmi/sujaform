110df9c51bd188d9575c56f4ad641fb0b198eb4f
AhmedScripter <ahmed.vibrant1@gmail.com>
2024-08-06 18:06:59 +0500

    changes added
    changes added

--- pages/api/bookingrequest/index.js
+++ pages/api/bookingrequest/index.js
@@ -3,7 +3,7 @@ import Lead from "@/database/models/Lead";
 
 export default async function GET(req, res) {
   let pageNo = req.query.page ? req.query.page : 0;
-  let limit = req.query.limit ? req.query.limit : 3;
+  let limit = req.query.limit ? req.query.limit : 5;
   let skip = pageNo * limit;
   console.log(pageNo, limit, skip);
 
@@ -20,11 +20,13 @@ export default async function GET(req, res) {
       const filteredLeads = leads.filter(
         lead => lead.step2.dr_course_type === 'regular' || lead.step2.dr_course_type === 'crash'
       );
+
+      if (filteredLeads.length > 0) {
         return res.json(filteredLeads);
+      }
     }
-    else {
+
     return res.status(404).send("No Leads Data");
-    }
   } catch (error) {
     console.error("Error fetching leads:", error);
     res.status(500).json({ success: false, error: "Internal Server Error" });
